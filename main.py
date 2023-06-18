7cfa45eeb5529a63ef60fd9f6e1b592dc5ed5036
DiegoRodrigues8800 <diegoluizxx@gmail.com>
2023-06-18 14:29:47 -0300

    CREATED FUNCTION 1,2,3 By Diego

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+lista_compras = []
+def funcao_1 ():
+    while True:
+        item = input("Digite o item ou tecle Enter para voltar: ")
+        if item == "":
+            break
+        quantidade = int(input("Qual a quantidade: "))
+        preco = float(input("Qual o preço: "))
+        produto = [item,quantidade,preco]
+        lista_compras.append(produto)
+        print("Item adicionado")
+def funcao_2():
+    for lista in lista_compras:
+        print(lista)
+def funcao_3():
+    soma = 0
+    for quantidade in lista_compras:
+        soma = soma + quantidade[1]
+    print(f"Você tem {soma} produtos no seu carrinho.")
 while True:
     print("-=-=-=Lista de Compras Supermercado=-=-=-")
     print("1. Primeira Função")
@@ -14,11 +33,26 @@ while True:
     escolha = int(input("Escolha uma opção: "))
     match escolha:
         case 1:
-            print("Primeira Função")
+            try:
+                funcao_1()
+            except Exception as ex:
+                print(f"Deu Erro qualquer: {ex}")
+            finally:
+                print("Processo terminado")
         case 2:
-            print("Segunda Função")
+            try:
+                funcao_2()
+            except Exception as ex:
+                print(f"Deu Erro qualquer: {ex}")
+            finally:
+                print("Processo terminado")
         case 3:
-            print("Terceira Função")
+            try:
+                funcao_3()
+            except Exception as ex:
+                print(f"Deu Erro qualquer: {ex}")
+            finally:
+                print("Processo terminado")
         case 4:
             print("Quarta Função")
         case 5:
